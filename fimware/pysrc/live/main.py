2089ecf1ed9f459e6d8ef411fd24bc715c19f4d4
Stefan Marx <stefan@marx-merten.de>
2025-02-01 12:56:14 +0000

    added basic micropython project

---
+++ fimware/pysrc/live/main.py
@@ -0,0 +1,9 @@
+# This is the main file that is run when the device boots up.
+# It is responsible for setting up the device and running the main loop.
+#
+
+# Default Logging
+import ulogging as logging
+LOG = logging.getLogger(__name__)
+
+LOG.info("STARTING MAIN")
